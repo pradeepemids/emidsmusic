44df28449f0886668a814974f464f77898fb2eff
SambaviEmids <sambaviv@emids.com>
2024-02-13 13:48:00 +0530

    Registration Login Changes

--- emids-music/src/Layout/Layout.js
+++ emids-music/src/Layout/Layout.js
@@ -14,59 +14,69 @@ import Songs from "../Playlist/Songs";
 import Test from "../Playlist/AddPlaylist";
 
 function Layout() {
+  const activeUser = JSON.parse(localStorage.getItem("activeUser"));
+  const index = activeUser.findIndex(
+    (user) => user.email === localStorage.getItem("currentUser")
+  );
+
   const logout = () => {
-    localStorage.setItem("isUserActive", false);
+    activeUser[index].isActive = false;
+    localStorage.setItem("activeUser", JSON.stringify(activeUser));
   };
 
-  return (
-    <div
-      style={{ display: "flex", height: "100%", backgroundColor: "#121212" }}>
-      <Sidebar className="app">
-        <Menu>
-          <MenuItem
-            className="menu1"
-            component={<Link to="dashboard" className="link" />}
-            icon={<img src={Logo} />}>
-            <h2>EMIDS MUSIC</h2>
-          </MenuItem>
-          <MenuItem
-            icon={<GridViewRoundedIcon />}
-            component={<NavLink to="dashboard" className="link" />}>
-            {" "}
-            Dashboard{" "}
-          </MenuItem>
-          <MenuItem
-            icon={<ReceiptRoundedIcon />}
-            component={<NavLink to="playlist" className="link" />}>
-            {" "}
-            My Playlist{" "}
-          </MenuItem>
-          <MenuItem
-            icon={<AccountCircleRoundedIcon />}
-            component={<NavLink to="profile" className="link" />}>
-            {" "}
-            Profile{" "}
-          </MenuItem>
-          <MenuItem
-            icon={<LogoutRoundedIcon />}
-            component={<NavLink to="/" className="link" onClick={logout} />}>
-            {" "}
-            Logout{" "}
-          </MenuItem>
-        </Menu>
-      </Sidebar>
-      <div className="main-div">
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="dashboard" element={<Dashboard />} />
-          <Route path="playlist" element={<Playlist />} />
-          <Route path="playlist/song" element={<Songs />} />
-          <Route path="playlist/add-playlist" element={<Test />} />
-          <Route path="profile" element={<UserProfile />} />
-        </Routes>
+  if (index === -1 || !activeUser[index].isActive) {
+    window.location.href = "/";
+  } else {
+    return (
+      <div
+        style={{ display: "flex", height: "100%", backgroundColor: "#121212" }}>
+        <Sidebar className="app">
+          <Menu>
+            <MenuItem
+              className="menu1"
+              component={<Link to="dashboard" className="link" />}
+              icon={<img src={Logo} />}>
+              <h2>EMIDS MUSIC</h2>
+            </MenuItem>
+            <MenuItem
+              icon={<GridViewRoundedIcon />}
+              component={<NavLink to="dashboard" className="link" />}>
+              {" "}
+              Dashboard{" "}
+            </MenuItem>
+            <MenuItem
+              icon={<ReceiptRoundedIcon />}
+              component={<NavLink to="playlist" className="link" />}>
+              {" "}
+              My Playlist{" "}
+            </MenuItem>
+            <MenuItem
+              icon={<AccountCircleRoundedIcon />}
+              component={<NavLink to="profile" className="link" />}>
+              {" "}
+              Profile{" "}
+            </MenuItem>
+            <MenuItem
+              icon={<LogoutRoundedIcon />}
+              component={<NavLink to="/" className="link" onClick={logout} />}>
+              {" "}
+              Logout{" "}
+            </MenuItem>
+          </Menu>
+        </Sidebar>
+        <div className="main-div">
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="playlist" element={<Playlist />} />
+            <Route path="playlist/song" element={<Songs />} />
+            <Route path="playlist/add-playlist" element={<PlayList />} />
+            <Route path="profile" element={<UserProfile />} />
+          </Routes>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default Layout;
