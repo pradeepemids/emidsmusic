99efe855719c8e711d79d682f292be847c3b4dc8
joshniEmids <joshnip@emids.com>
2024-02-26 15:23:02 +0530

    AudioOverlapChanges

--- emids-music/src/Shared/SongCard.jsx
+++ emids-music/src/Shared/SongCard.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 import { CircularProgress } from "@mui/material";
 
-function SongCard({ songsdata }) {
+function SongCard({ songsdata, parentFunction }) { 
+  const pauseAudio = (event) => {
+    parentFunction(event.target.id);
+
+  }
+
   if (!songsdata && !!songsdata.length) {
     return <CircularProgress />;
   } else {
@@ -15,7 +20,7 @@ function SongCard({ songsdata }) {
             className="card-img-top"
             alt="..."
           />
-          <div className="card-body">
+          <div className="card-body" >
             <h5
               className="card-title"
               style={{
@@ -27,13 +32,11 @@ function SongCard({ songsdata }) {
               {songsdata.title}
             </h5>
             <p className="card-text">Artist: {songsdata.artist.name}</p>
-            {
-              <audio
-                src={songsdata.preview}
-                controls
-                className="audio-play"
-                style={{ width: "230px" }}></audio>
-            }
+            <audio id={"audio-id" + songsdata.album.id}
+              src={songsdata.preview}
+              className="audio-play"
+              controls
+              style={{ width: "230px" }} onPlay={pauseAudio} ></audio>
           </div>
         </div>
       </div>
